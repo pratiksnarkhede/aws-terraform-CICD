13cc8e31d883c005c2ef5f8081a17259949a2681
Pratik Narkhede <89267289+pratiksnarkhede@users.noreply.github.com>
2024-01-06 22:52:44 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,7 +3,7 @@ variable "myregion" {
 }
 
 variable "accountId" {
-  default = "438233270299"
+  default = "003472343294"
 }
 
 
@@ -14,11 +14,11 @@ variable "custom_domain_name" {
 
 # Define your existing SSL certificate ARN
 variable "ssl_certificate_arn" {
-  default = "arn:aws:acm:us-east-1:438233270299:certificate/01cbf241-03ed-4d6f-a505-133f2321c8ed"  # Replace with your SSL certificate ARN
+  default = "arn:aws:acm:us-east-1:003472343294:certificate/965fccb0-79c2-409c-ab84-9cd1164f2cfa"  # Replace with your SSL certificate ARN
 }
 
 # Define your hosted zone ID
 variable "hosted_zone_id" {
-  default = "Z028009242LYGS6EC2PA"  # Replace with your hosted zone ID
+  default = "Z097808527FMTY6LBEHZQ"  # Replace with your hosted zone ID
 }
 
