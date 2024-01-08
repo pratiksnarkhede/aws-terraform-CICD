eee8bbea9efb162613c0edeaf56d26f61b79047d
Pratik Narkhede <89267289+pratiksnarkhede@users.noreply.github.com>
2024-01-08 12:01:43 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ sudo apt-get install apache2 -y
 sudo /etc/init.d/apache2 start
 sudo systemctl enable apache2
 cd /home/ubuntu
-sudo git clone git@github.com:pratiksnarkhede/Frontend-code-api.git
+sudo git clone https://github.com/pratiksnarkhede/Frontend-code-api.git
 sudo rm -r /var/www/html/index.html 
 sudo cp /home/ubuntu/Frontend-code-api/* /var/www/html
 
